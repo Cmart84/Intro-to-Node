50bf2aa883ca4c55d876c25bff153c0395488174
Cory Martin <corymartin84@hotmail.com>
2023-03-08 16:16:38 -0600

    updated package.json, prcfile, and hello.js PORT

--- Hello.js
+++ Hello.js
@@ -1,6 +1,9 @@
 var http = require('http');
 var fs = require('fs');
 var url = require('url');
+const PORT = process.env.PORT || 5000
+
+
 
 http.createServer(function (req, res){
 var q = url.parse(req.url, true);
@@ -19,7 +22,7 @@ res.writeHead(200, {'Content-Type': 'text/html'});
 res.write(data);
 return res.end();
 });
-}).listen(8080);
+}).listen(PORT);
 
 
 console.log("Server Listening on Port 8080");
